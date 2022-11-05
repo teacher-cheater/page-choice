3c7e88eebf8b1d45de6e021f517d72dce8aed5fe
teacher_cheater <johnnykayfun@gmail.com>
2022-11-05 20:57:05 +0300

    add card

--- js/app.js
+++ js/app.js
@@ -41,3 +41,13 @@ document.querySelectorAll('.dropdown').forEach((dropDownWrapper) => {
 });
 
 //------------slider---------
+document.querySelectorAll('radio').forEach((silderImg) => {
+  let counter = 1;
+  setInterval(() => {
+    document.getElementById(`radio${counter}`).checked = true;
+    counter++;
+    if (counter > 4) {
+      counter = 1;
+    }
+  }, 3000);
+});
