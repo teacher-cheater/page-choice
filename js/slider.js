b083f1976ff75632153712ae923fc79c0eb71dd9
teacher_cheater <johnnykayfun@gmail.com>
2022-11-07 02:38:01 +0300

    add slider swiper

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,4 @@
 //------------slider---------
-//document.querySelectorAll('radio').forEach((silderImg) => {
 let counter = 1;
 setInterval(() => {
   document.getElementById(`radio${counter}`).checked = true;
@@ -8,4 +7,41 @@ setInterval(() => {
     counter = 1;
   }
 }, 5000);
-  //});
+
+//----slider-swiper
+new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+    pageUpDown: true,
+  },
+  autoHeight: true,
+  watchOverfloy: true,
+  autoplay: {
+    delay: 3000,
+  },
+  speed: 300,
+  effect: 'fade',
+  breakpoints: {
+    320: {
+      slidePerView: 1,
+    },
+    480: {
+      slidePerView: 1,
+    },
+    992: {
+      slidePerView: 1,
+    },
+  },
+  preloadImages: false,
+  lazy: {
+    loadOnTransitionStart: false,
+    loadPrevNext: false,
+  },
+  watchSlidesProgress: true,
+  watchSlidesVisibility: true,
+})
