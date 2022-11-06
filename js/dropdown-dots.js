aaf4df3cf24be27d6839098f8db370aa67758054
teacher_cheater <johnnykayfun@gmail.com>
2022-11-07 02:06:14 +0300

    fix bugs

--- js/dropdown-dots.js
+++ js/dropdown-dots.js
@@ -1,20 +1,38 @@
-const dropDownBtnDots = document.querySelector('.dropdown-dots__btn');
-const dropDownDotsList = document.querySelector('.dropdown-dots__list');
-const dropDownDotsListItems = dropDownDotsList.querySelectorAll('.dropdown-dots__item');
+document.querySelectorAll('.dropdown-dots').forEach((dropDownWrapper) => {
+  const dropDownBtn = dropDownWrapper.querySelector('.dropdown-dots__btn');
+  const dropDownList = dropDownWrapper.querySelector('.dropdown-dots__list');
+  const dropDownListItems = dropDownList.querySelectorAll('.dropdown-dots__item');
+  const dropDownInput = dropDownWrapper.querySelector('.dropdown-dots__input');
 
-// Клик по кнопке. Открыть/Закрыть select-dots
-dropDownBtnDots.addEventListener('click', function (e) {
-  dropDownDotsList.classList.toggle('dropdown-dots__list--visible');
-  this.classList.add('dropdown-items__btn--active');
-});
+  // Клик по кнопке. Открыть/Закрыть select
+  dropDownBtn.addEventListener('click', function (e) {
+    dropDownList.classList.toggle('dropdown-dots__list--visible');
+    this.classList.add('dropdown-dots__btn--active');
+  });
 
-// Выбор элемента списка dots. Запомнить выбранное значение. Закрыть дропдаун
-dropDownDotsListItems.forEach((listItem) => {
-  listItem.addEventListener('click', (e) => {
-    e.stopPropagation();
-    //dropDownBtnDots.innerText = this.innerText;
-    dropDownBtnDots.focus();
-    //dropDownInput.value = this.dataset.value;
-    dropDownDotsList.classList.remove('dropdown-dots__list--visible');
+  // Выбор элемента списка. Запомнить выбранное значение. Закрыть дропдаун
+  dropDownListItems.forEach((listItem) => {
+    listItem.addEventListener('click', function (e) {
+      e.stopPropagation();
+      dropDownBtn.focus();
+      dropDownInput.value = this.dataset.value;
+      dropDownList.classList.remove('dropdown-dots__list--visible');
+    });
+  });
+
+  // Клик снаружи дропдауна. Закрыть дропдаун
+  document.addEventListener('click', (e) => {
+    if (e.target !== dropDownBtn) {
+      dropDownBtn.classList.remove('dropdown-dots__btn--active');
+      dropDownList.classList.remove('dropdown-dots__list--visible');
+    }
+  });
+
+  // Нажатие на Tab или Escape. Закрыть дропдаун
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Tab' || e.key === 'Escape') {
+      dropDownBtn.classList.remove('dropdown-dots__btn--active');
+      dropDownList.classList.remove('dropdown-dots__list--visible');
+    }
   });
 });
